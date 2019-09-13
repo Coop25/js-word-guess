3ef668b272ace4521585f768638abad27126e5ab
Adam Cooper <frankidyfrank3@gmal.com>
2019-09-13 18:30:01 -0500

    created: function for letter.js file

---
+++ Letter.js
@@ -0,0 +1,20 @@
+module.exports.letter = function(letter){
+    this.letter = letter;
+    this.isGuessed = false;
+    this.display = function(){
+        switch(this.isGuessed){
+            case true:
+                return this.letter;
+                break;
+            case false:
+                return "_";
+                break;
+        }
+    }
+    this.checkChar = function(guess){
+        if (guess === this.letter){
+            this.isGuessed = true;
+        }
+        return;
+    }
+}
